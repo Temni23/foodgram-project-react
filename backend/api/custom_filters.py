de8bcf9f2a40dcca54a046049c56072faa2f0ab3
Temni23 <temni23@yandex.ru>
2023-08-05 10:18:31 +0700

    Добавлены actions github.

--- backend/api/custom_filters.py
+++ backend/api/custom_filters.py
@@ -1,16 +1,26 @@
 import django_filters
-from django_filters import filterset
+from django_filters import FilterSet, filters
+from django_filters.widgets import BooleanWidget
 
 from foodgram.models import Recipe, Tag
 
 
-class RecipeFilter(filterset.FilterSet):
+class RecipeFilter(FilterSet):
     tags = django_filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
+    is_favorited = filters.BooleanFilter(
+        widget=BooleanWidget(),
+        method='filter_is_favorited')
 
     class Meta:
         model = Recipe
-        fields = ['tags__slug']
+        fields = ['tags__slug', 'is_favorited']
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value:
+            return queryset.filter(recipe_favorites__user=self.request.user)
+        return queryset
+
