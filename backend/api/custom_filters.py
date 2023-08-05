a92c5ef48e84cd48f0ec95a996b088d5fdcce581
Temni23 <temni23@yandex.ru>
2023-08-05 13:11:31 +0700

    Косметические правки.

--- backend/api/custom_filters.py
+++ backend/api/custom_filters.py
@@ -12,12 +12,20 @@ class RecipeFilter(FilterSet):
     is_favorited = filters.BooleanFilter(
         widget=widgets.BooleanWidget(),
         method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        widget=widgets.BooleanWidget(),
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ['tags__slug', 'is_favorited']
+        fields = ['tags__slug', 'is_favorited', 'is_in_shopping_cart']
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
             return queryset.filter(recipe_favorites__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(cart_recipes__user=self.request.user)
         return queryset
