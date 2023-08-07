448b46350fdb5376d0d31eb05cab2457edc2afb9
Temni23 <temni23@yandex.ru>
2023-08-07 13:08:09 +0700

    Исправлен скрипт импорта ингредиентов.

--- backend/foodgram/management/commands/import_ingredients.py
+++ backend/foodgram/management/commands/import_ingredients.py
@@ -7,13 +7,17 @@ from foodgram.models import Ingredient
 class Command(BaseCommand):
     help = 'Import ingredients from CSV file'
 
+
     def handle(self, *args, **options):
+        ingredients_list = []
         file_path = 'data/ingredients.csv'
         with open(file_path, 'r', encoding='utf-8') as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
                 ingredient = Ingredient(name=row['абрикосовое варенье'],
                                         measurement_unit=row['г'])
-                ingredient.save()
+                ingredients_list.append(ingredient)
+            Ingredient.objects.bulk_create(ingredients_list)
+
         self.stdout.write(
             self.style.SUCCESS('Ingredient imported successfully'))
