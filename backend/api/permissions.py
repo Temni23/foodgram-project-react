a266fc65138637b567805b71a3e156b21f22e04e
Artem Klimov <temni23@yandex.ru>
2023-09-07 20:48:45 +0700

    Fixed flake8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,7 +3,9 @@ from rest_framework import permissions
 
 class IsAuthorStaffOrReadOnly(permissions.BasePermission):
     """Разрешение дает доступ для редактирования автору, администратору
-        для остальных только чтение"""
+
+    для остальных только чтение
+    """
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
